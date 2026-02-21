76134398b815aad032045530f14e48b4ff054a4a
sjthesahiljoseph <sjthesahiljoseph@gmail.com>
2026-02-21 11:46:34 +0500

    ..

--- init.el
+++ init.el
@@ -124,11 +124,6 @@
     :defer t
     )
 
-(use-package javascript-mode
-    :ensure t
-    :defer t
-    )
-
 (use-package json-mode
     :ensure t
     :defer t
@@ -317,3 +312,15 @@
 
 
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
